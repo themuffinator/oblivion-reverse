b990eb0e9218518a1e90e45a61bd325fde38d6d5
themuffinator <djdacosta86@yahoo.co.uk>
2025-11-06 23:34:21 +0000

    Restore cyborg and spider attack scheduling and audio

--- src/game/m_cyborg.c
+++ src/game/m_cyborg.c
@@ -10,13 +10,21 @@
 
 #include "g_local.h"
 
+#ifndef ARRAY_LEN
+#define ARRAY_LEN(x) (sizeof (x) / sizeof ((x)[0]))
+#endif
+
 #define CYBORG_FRAME_STAND     0
 #define CYBORG_FRAME_WALK_START        1
 #define CYBORG_FRAME_WALK_END  4
 #define CYBORG_FRAME_RUN_START         5
 #define CYBORG_FRAME_RUN_END   8
-#define CYBORG_FRAME_ATTACK_START      9
-#define CYBORG_FRAME_ATTACK_END        12
+#define CYBORG_FRAME_ATTACK_PRIMARY_START      24
+#define CYBORG_FRAME_ATTACK_PRIMARY_END        35
+#define CYBORG_FRAME_ATTACK_ALT_START           47
+#define CYBORG_FRAME_ATTACK_ALT_END             52
+#define CYBORG_FRAME_ATTACK_FINISH_START        53
+#define CYBORG_FRAME_ATTACK_FINISH_END          58
 #define CYBORG_FRAME_PAIN_START        13
 #define CYBORG_FRAME_PAIN_END  14
 #define CYBORG_FRAME_DEATH_START       15
@@ -26,13 +34,19 @@ static int sound_sight;
 static int sound_search;
 static int sound_idle;
 static int sound_step[3];
-static int sound_pain;
+static int sound_pain[2];
 static int sound_death;
-static int sound_attack;
+static int sound_attack[3];
+static int sound_land;
 
 static void cyborg_step (edict_t *self)
 {
     gi.sound (self, CHAN_BODY, sound_step[rand () % 3], 1, ATTN_NORM, 0);
+}
+
+static void cyborg_land (edict_t *self)
+{
+    gi.sound (self, CHAN_BODY, sound_land, 1, ATTN_NORM, 0);
 }
 
 static void cyborg_idle (edict_t *self)
@@ -69,7 +83,7 @@ static void cyborg_fire_deatom (edict_t *self)
     VectorSubtract (target, start, dir);
     VectorNormalize (dir);
 
-    gi.sound (self, CHAN_WEAPON, sound_attack, 1, ATTN_NORM, 0);
+    gi.sound (self, CHAN_WEAPON, sound_attack[rand () % 3], 1, ATTN_NORM, 0);
 
     // The original DLL rolled cyborg deatom damage from a narrow band per shot
     // before spawning a high-speed tracking projectile.  Match that behaviour
@@ -90,10 +104,24 @@ static void cyborg_fire_deatom (edict_t *self)
     }
 }
 
-static void cyborg_attack_think (edict_t *self)
+static void cyborg_attack_fire (edict_t *self)
 {
     if (visible (self, self->enemy) && range (self, self->enemy) <= RANGE_FAR)
         cyborg_fire_deatom (self);
+}
+
+static float cyborg_attack_duration (const mmove_t *move)
+{
+    return (float)((move->lastframe - move->firstframe) + 1) * FRAMETIME;
+}
+
+static void cyborg_attack_recover (edict_t *self)
+{
+    if (self->monsterinfo.attack_finished < level.time + 0.2f)
+        self->monsterinfo.attack_finished = level.time + 0.2f;
+
+    if (self->monsterinfo.run)
+        self->monsterinfo.run (self);
 }
 
 static mframe_t cyborg_frames_stand[] = {
@@ -117,13 +145,56 @@ static mframe_t cyborg_frames_run[] = {
 };
 static mmove_t cyborg_move_run = {CYBORG_FRAME_RUN_START, CYBORG_FRAME_RUN_END, cyborg_frames_run, NULL};
 
-static mframe_t cyborg_frames_attack[] = {
+static mframe_t cyborg_frames_attack_primary[] = {
+    {ai_charge, 0, cyborg_land},
     {ai_charge, 0, NULL},
-    {ai_charge, 0, cyborg_attack_think},
+    {ai_charge, 0, cyborg_attack_fire},
     {ai_charge, 0, NULL},
-    {ai_charge, 0, cyborg_attack_think}
+    {ai_charge, 0, cyborg_attack_fire},
+    {ai_charge, 0, NULL},
+    {ai_charge, 0, cyborg_attack_fire},
+    {ai_charge, 0, NULL},
+    {ai_charge, 0, cyborg_attack_fire},
+    {ai_charge, 0, NULL},
+    {ai_charge, 0, cyborg_attack_fire},
+    {ai_charge, 0, NULL}
 };
-static mmove_t cyborg_move_attack = {CYBORG_FRAME_ATTACK_START, CYBORG_FRAME_ATTACK_END, cyborg_frames_attack, NULL};
+static mmove_t cyborg_move_attack_primary = {
+    CYBORG_FRAME_ATTACK_PRIMARY_START,
+    CYBORG_FRAME_ATTACK_PRIMARY_END,
+    cyborg_frames_attack_primary,
+    cyborg_attack_recover
+};
+
+static mframe_t cyborg_frames_attack_alt[] = {
+    {ai_charge, 0, cyborg_land},
+    {ai_charge, 0, NULL},
+    {ai_charge, 0, cyborg_attack_fire},
+    {ai_charge, 0, NULL},
+    {ai_charge, 0, cyborg_attack_fire},
+    {ai_charge, 0, NULL}
+};
+static mmove_t cyborg_move_attack_alt = {
+    CYBORG_FRAME_ATTACK_ALT_START,
+    CYBORG_FRAME_ATTACK_ALT_END,
+    cyborg_frames_attack_alt,
+    cyborg_attack_recover
+};
+
+static mframe_t cyborg_frames_attack_finisher[] = {
+    {ai_charge, 0, cyborg_land},
+    {ai_charge, 0, NULL},
+    {ai_charge, 0, cyborg_attack_fire},
+    {ai_charge, 0, cyborg_step},
+    {ai_charge, 0, cyborg_attack_fire},
+    {ai_charge, 0, NULL}
+};
+static mmove_t cyborg_move_attack_finisher = {
+    CYBORG_FRAME_ATTACK_FINISH_START,
+    CYBORG_FRAME_ATTACK_FINISH_END,
+    cyborg_frames_attack_finisher,
+    cyborg_attack_recover
+};
 
 static void cyborg_stand (edict_t *self)
 {
@@ -140,10 +211,29 @@ static void cyborg_run (edict_t *self)
     self->monsterinfo.currentmove = &cyborg_move_run;
 }
 
+typedef struct
+{
+    mmove_t *move;
+    float    cooldown;
+} cyborg_attack_choice_t;
+
+static void cyborg_begin_attack (edict_t *self, const cyborg_attack_choice_t *choice)
+{
+    self->monsterinfo.currentmove = choice->move;
+    self->monsterinfo.attack_finished = level.time + cyborg_attack_duration (choice->move) + choice->cooldown;
+}
+
 static void cyborg_attack (edict_t *self)
 {
-    self->monsterinfo.currentmove = &cyborg_move_attack;
-    self->monsterinfo.attack_finished = level.time + 1.2f;
+    static const cyborg_attack_choice_t attack_table[] = {
+        {&cyborg_move_attack_primary, 0.2f},
+        {&cyborg_move_attack_alt, 0.3f},
+        {&cyborg_move_attack_finisher, 0.35f}
+    };
+    const cyborg_attack_choice_t *choice;
+
+    choice = &attack_table[rand () % ARRAY_LEN (attack_table)];
+    cyborg_begin_attack (self, choice);
 }
 
 static void cyborg_pain (edict_t *self, edict_t *other, float kick, int damage)
@@ -157,8 +247,8 @@ static void cyborg_pain (edict_t *self, edict_t *other, float kick, int damage)
     if (level.time < self->pain_debounce_time)
         return;
 
-    self->pain_debounce_time = level.time + 2.0f;
-    gi.sound (self, CHAN_VOICE, sound_pain, 1, ATTN_NORM, 0);
+    self->pain_debounce_time = level.time + 3.0f;
+    gi.sound (self, CHAN_VOICE, sound_pain[rand () & 1], 1, ATTN_NORM, 0);
     self->monsterinfo.currentmove = &pain_move;
 }
 
@@ -209,12 +299,16 @@ void SP_monster_cyborg (edict_t *self)
     sound_sight = gi.soundindex ("cyborg/mutsght1.wav");
     sound_search = gi.soundindex ("cyborg/mutsrch1.wav");
     sound_idle = gi.soundindex ("cyborg/mutidle1.wav");
-    sound_pain = gi.soundindex ("cyborg/mutpain1.wav");
+    sound_pain[0] = gi.soundindex ("cyborg/mutpain1.wav");
+    sound_pain[1] = gi.soundindex ("cyborg/mutpain2.wav");
     sound_death = gi.soundindex ("cyborg/mutdeth1.wav");
-    sound_attack = gi.soundindex ("deatom/dfire.wav");
+    sound_attack[0] = gi.soundindex ("cyborg/mutatck1.wav");
+    sound_attack[1] = gi.soundindex ("cyborg/mutatck2.wav");
+    sound_attack[2] = gi.soundindex ("cyborg/mutatck3.wav");
     sound_step[0] = gi.soundindex ("cyborg/step1.wav");
     sound_step[1] = gi.soundindex ("cyborg/step2.wav");
     sound_step[2] = gi.soundindex ("cyborg/step3.wav");
+    sound_land = gi.soundindex ("mutant/thud1.wav");
 
     self->s.sound = gi.soundindex ("cyborg/mutidle1.wav");
 
--- src/game/m_spider.c
+++ src/game/m_spider.c
@@ -7,25 +7,40 @@
 
 #include "g_local.h"
 
+#ifndef ARRAY_LEN
+#define ARRAY_LEN(x) (sizeof (x) / sizeof ((x)[0]))
+#endif
+
 #define SPIDER_FRAME_STAND      0
 #define SPIDER_FRAME_WALK_START         1
 #define SPIDER_FRAME_WALK_END   6
 #define SPIDER_FRAME_RUN_START          7
 #define SPIDER_FRAME_RUN_END    12
-#define SPIDER_FRAME_ATTACK_START       13
-#define SPIDER_FRAME_ATTACK_END 18
+#define SPIDER_FRAME_ATTACK_PRIMARY_START       65
+#define SPIDER_FRAME_ATTACK_PRIMARY_END         74
+#define SPIDER_FRAME_ATTACK_ALT_START           75
+#define SPIDER_FRAME_ATTACK_ALT_END             80
+#define SPIDER_FRAME_ATTACK_FINISH_START        99
+#define SPIDER_FRAME_ATTACK_FINISH_END          103
 #define SPIDER_FRAME_PAIN_START         19
 #define SPIDER_FRAME_PAIN_END   21
 #define SPIDER_FRAME_DEATH_START        22
 #define SPIDER_FRAME_DEATH_END  28
 
+#define SPIDER_COMBO_PENDING    1
+
 static int sound_sight;
 static int sound_search;
 static int sound_idle;
-static int sound_pain;
+static int sound_pain[2];
 static int sound_death;
 static int sound_melee[3];
-static int sound_thud;
+static int sound_step;
+static int sound_land;
+
+static mmove_t spider_move_attack_primary;
+static mmove_t spider_move_attack_alt;
+static mmove_t spider_move_attack_finish;
 
 static void spider_idle (edict_t *self)
 {
@@ -45,7 +60,12 @@ static void spider_search (edict_t *self)
 
 static void spider_step (edict_t *self)
 {
-    gi.sound (self, CHAN_BODY, sound_thud, 1, ATTN_NORM, 0);
+    gi.sound (self, CHAN_BODY, sound_step, 1, ATTN_NORM, 0);
+}
+
+static void spider_land (edict_t *self)
+{
+    gi.sound (self, CHAN_BODY, sound_land, 1, ATTN_NORM, 0);
 }
 
 static void spider_claw (edict_t *self)
@@ -63,6 +83,62 @@ static void spider_claw (edict_t *self)
 
     gi.sound (self, CHAN_WEAPON, sound_melee[rand () % 3], 1, ATTN_NORM, 0);
     T_Damage (self->enemy, self, self, forward, self->enemy->s.origin, vec3_origin, (int)damage, (int)damage, 0, MOD_HIT);
+}
+
+static float spider_attack_duration (const mmove_t *move)
+{
+    return (float)((move->lastframe - move->firstframe) + 1) * FRAMETIME;
+}
+
+static void spider_begin_attack (edict_t *self, const mmove_t *move, float cooldown)
+{
+    self->monsterinfo.attack_state = 0;
+    self->monsterinfo.pausetime = 0;
+    self->monsterinfo.currentmove = (mmove_t *) move;
+    self->monsterinfo.attack_finished = level.time + spider_attack_duration (move) + cooldown;
+}
+
+static void spider_combo_mark (edict_t *self)
+{
+    if (!self->enemy)
+        return;
+
+    if (range (self, self->enemy) > RANGE_MELEE)
+        return;
+
+    self->monsterinfo.attack_state = SPIDER_COMBO_PENDING;
+    self->monsterinfo.pausetime = level.time + 0.6f;
+}
+
+static void spider_finish_attack (edict_t *self)
+{
+    if (self->monsterinfo.attack_finished < level.time + 0.4f)
+        self->monsterinfo.attack_finished = level.time + 0.4f;
+
+    if (self->monsterinfo.run)
+        self->monsterinfo.run (self);
+}
+
+static void spider_try_chain (edict_t *self)
+{
+    if (self->monsterinfo.attack_state == SPIDER_COMBO_PENDING)
+    {
+        if (level.time <= self->monsterinfo.pausetime)
+        {
+            spider_begin_attack (self, &spider_move_attack_finish, 0.35f);
+            return;
+        }
+
+        self->monsterinfo.attack_state = 0;
+    }
+
+    spider_finish_attack (self);
+}
+
+static void spider_cancel_combo (edict_t *self)
+{
+    self->monsterinfo.attack_state = 0;
+    self->monsterinfo.pausetime = 0;
 }
 
 static mframe_t spider_frames_stand[] = {
@@ -90,15 +166,53 @@ static mframe_t spider_frames_run[] = {
 };
 static mmove_t spider_move_run = {SPIDER_FRAME_RUN_START, SPIDER_FRAME_RUN_END, spider_frames_run, NULL};
 
-static mframe_t spider_frames_attack[] = {
-    {ai_charge, 0, spider_claw},
+static mframe_t spider_frames_attack_primary[] = {
+    {ai_charge, 0, spider_land},
     {ai_charge, 0, NULL},
     {ai_charge, 0, spider_claw},
     {ai_charge, 0, NULL},
+    {ai_charge, 0, spider_step},
+    {ai_charge, 0, spider_claw},
+    {ai_charge, 0, NULL},
+    {ai_charge, 0, spider_combo_mark},
     {ai_charge, 0, spider_claw},
     {ai_charge, 0, NULL}
 };
-static mmove_t spider_move_attack = {SPIDER_FRAME_ATTACK_START, SPIDER_FRAME_ATTACK_END, spider_frames_attack, NULL};
+static mmove_t spider_move_attack_primary = {
+    SPIDER_FRAME_ATTACK_PRIMARY_START,
+    SPIDER_FRAME_ATTACK_PRIMARY_END,
+    spider_frames_attack_primary,
+    spider_try_chain
+};
+
+static mframe_t spider_frames_attack_alt[] = {
+    {ai_charge, 0, spider_land},
+    {ai_charge, 0, NULL},
+    {ai_charge, 0, spider_claw},
+    {ai_charge, 0, spider_step},
+    {ai_charge, 0, spider_claw},
+    {ai_charge, 0, spider_combo_mark}
+};
+static mmove_t spider_move_attack_alt = {
+    SPIDER_FRAME_ATTACK_ALT_START,
+    SPIDER_FRAME_ATTACK_ALT_END,
+    spider_frames_attack_alt,
+    spider_try_chain
+};
+
+static mframe_t spider_frames_attack_finish[] = {
+    {ai_charge, 0, spider_land},
+    {ai_charge, 0, NULL},
+    {ai_charge, 0, spider_claw},
+    {ai_charge, 0, NULL},
+    {ai_charge, 0, spider_claw}
+};
+static mmove_t spider_move_attack_finish = {
+    SPIDER_FRAME_ATTACK_FINISH_START,
+    SPIDER_FRAME_ATTACK_FINISH_END,
+    spider_frames_attack_finish,
+    spider_finish_attack
+};
 
 static void spider_stand (edict_t *self)
 {
@@ -117,8 +231,24 @@ static void spider_run (edict_t *self)
 
 static void spider_attack (edict_t *self)
 {
-    self->monsterinfo.currentmove = &spider_move_attack;
-    self->monsterinfo.attack_finished = level.time + 1.0f;
+    if (self->monsterinfo.attack_state == SPIDER_COMBO_PENDING)
+    {
+        if (level.time <= self->monsterinfo.pausetime)
+        {
+            spider_begin_attack (self, &spider_move_attack_finish, 0.35f);
+            return;
+        }
+
+        spider_cancel_combo (self);
+    }
+
+    if (level.time < self->monsterinfo.attack_finished)
+        return;
+
+    if (random () < 0.5f)
+        spider_begin_attack (self, &spider_move_attack_primary, 0.25f);
+    else
+        spider_begin_attack (self, &spider_move_attack_alt, 0.3f);
 }
 
 static void spider_pain (edict_t *self, edict_t *other, float kick, int damage)
@@ -134,7 +264,9 @@ static void spider_pain (edict_t *self, edict_t *other, float kick, int damage)
         return;
 
     self->pain_debounce_time = level.time + 1.5f;
-    gi.sound (self, CHAN_VOICE, sound_pain, 1, ATTN_NORM, 0);
+    gi.sound (self, CHAN_VOICE, sound_pain[self->monsterinfo.lefty & 1], 1, ATTN_NORM, 0);
+    self->monsterinfo.lefty ^= 1;
+    spider_cancel_combo (self);
     self->monsterinfo.currentmove = &pain_move;
 }
 
@@ -156,6 +288,8 @@ static void spider_die (edict_t *self, edict_t *inflictor, edict_t *attacker, in
         {ai_move, 0, spider_dead}
     };
     static mmove_t death_move = {SPIDER_FRAME_DEATH_START, SPIDER_FRAME_DEATH_END, death_frames, spider_dead};
+
+    spider_cancel_combo (self);
 
     gi.sound (self, CHAN_VOICE, sound_death, 1, ATTN_NORM, 0);
 
@@ -189,12 +323,14 @@ void SP_monster_spider (edict_t *self)
     sound_sight = gi.soundindex ("spider/sight.wav");
     sound_search = gi.soundindex ("gladiator/gldsrch1.wav");
     sound_idle = gi.soundindex ("gladiator/gldidle1.wav");
-    sound_pain = gi.soundindex ("gladiator/pain.wav");
+    sound_pain[0] = gi.soundindex ("gladiator/pain.wav");
+    sound_pain[1] = gi.soundindex ("gladiator/gldpain2.wav");
     sound_death = gi.soundindex ("mutant/thud1.wav");
     sound_melee[0] = gi.soundindex ("gladiator/melee1.wav");
     sound_melee[1] = gi.soundindex ("gladiator/melee2.wav");
     sound_melee[2] = gi.soundindex ("gladiator/melee3.wav");
-    sound_thud = gi.soundindex ("mutant/thud1.wav");
+    sound_step = gi.soundindex ("mutant/thud1.wav");
+    sound_land = gi.soundindex ("mutant/thud1.wav");
 
     self->s.sound = sound_idle;
 
